37ae1454d96f752bfa2e46772f55ac5b7777677e
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-10-17 11:57:17 -0700

    test weekly calendar on home screen

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -38,6 +38,19 @@ const userList = useSelector( (state) => state.users.users);
 const currUser = userList.filter(user => user.username == authToken.email);
 const behaviorList = currUser[0].behaviors;
 
+const sampleEvents = [
+  { 'start': '2023-07-17 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
+  { 'start': '2020-03-24 14:00:00', 'duration': '01:00:00', 'note': 'Doctor\'s appointment' },
+  { 'start': '2020-03-25 08:00:00', 'duration': '00:30:00', 'note': 'Morning exercise' },
+  { 'start': '2020-03-25 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' },
+  { 'start': '2020-03-25 19:00:00', 'duration': '01:00:00', 'note': 'Dinner with family' },
+  { 'start': '2020-03-26 09:30:00', 'duration': '01:00:00', 'note': 'Schedule 1' },
+  { 'start': '2020-03-26 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
+  { 'start': '2020-03-26 15:00:00', 'duration': '01:30:00', 'note': 'Schedule 3' },
+  { 'start': '2020-03-26 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
+  { 'start': '2020-03-26 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' }
+]
+
 
   const [ date, setDate ] = useState(null);
 
@@ -84,6 +97,7 @@ const behaviorList = currUser[0].behaviors;
   }
 
   return (
+    /*
     <>
     <StatusBar style="light"/>
     <View style={styles.appContainer}>
@@ -125,6 +139,12 @@ const behaviorList = currUser[0].behaviors;
         </View>
       </View>
         </>
+*/
+
+<View style={styles.container}>
+<WeeklyCalendar events={sampleEvents} style={{ }} />
+</View>
+
   );
 }
 
