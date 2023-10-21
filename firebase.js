00fb365a8964593c0225b25055edb17486c12da3
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-10-20 19:34:35 -0700

    installed firebase

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUuZUgvlmMYBOl_Kg43Eb2sHnEJp4_us4",
+  authDomain: "sandler-cookbook.firebaseapp.com",
+  projectId: "sandler-cookbook",
+  storageBucket: "sandler-cookbook.appspot.com",
+  messagingSenderId: "519878306197",
+  appId: "1:519878306197:web:4fe836d84e5caae16dae9c",
+  measurementId: "G-PF1Q3WHM3P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
