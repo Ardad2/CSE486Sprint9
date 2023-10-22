df0de30ee4683e00fdaf17d36c81dd4512c2460e
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-10-22 15:46:52 -0700

    restore old sign up screen

--- screens/AuthScreen/SignupScreen.js
+++ screens/AuthScreen/SignupScreen.js
@@ -1,4 +1,3 @@
-// The code in this file handles user registration and authentication
 import { useState } from 'react';
 import AuthContent from '../../components/Auth/AuthContent';
 import LoadingOverlay from '../../components/ui/LoadingOverlay';
@@ -6,23 +5,21 @@ import {createUser} from '../../util/auth';
 import { Alert } from 'react-native';
 import { useSelector, dispatch , useDispatch} from 'react-redux';
 import { authenticateAuthTokens, logoutAuthTokens } from '../../store/redux/authTokens';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import {auth} from '../../firebase';
 
 
 function SignupScreen() {
-  const [isAuthenticating, setIsAuthenticating] = useState(false); // Set useState to false because this application/screen is not currently authenticating a user
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   const dispatch = useDispatch();
 
   const authToken = useSelector( (state) => state.authTokens.data[0]); 
 
-  // This function serves as an event handler for when a user is attempting to sign up
+
   async function signupHandler({ email, password }) {
-    setIsAuthenticating(true); // Set this to true because now the application is actively authenticating and signing the user in
+    setIsAuthenticating(true);
     try {
       const data = await createUser(email, password);
-       
+      
       console.log(data.idToken);
 
       dispatch(addUser(
@@ -30,9 +27,7 @@ function SignupScreen() {
           username: data.email,
         }
       ));
-
-
-      createUserWithEmailAndPassword(auth,email,password); // Create the user with the authenticated email and password
+       
       
 
       dispatch(authenticateAuthTokens(
@@ -41,7 +36,7 @@ function SignupScreen() {
           email: data.email
         }
       ));
-    // Purpose of this catch block is to catch and log if there is an error in the user's authentication
+
     }catch (error) {
       console.log(error);
       Alert.alert(
@@ -52,13 +47,11 @@ function SignupScreen() {
     setIsAuthenticating(false);
   }
 
-  // If the user was successfully authenticated (isAuthenticating == true)
   if (isAuthenticating) {
-
-    return <LoadingOverlay message="Creating user..." />; // Then return a message that the user is being created and the signup is being processed
+    return <LoadingOverlay message="Creating user..." />;
   }
 
-  return <AuthContent onAuthenticate={signupHandler} />; 
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
