5eda0338ce5fbc7506bed0a1b11e824831397425
jennalei02 <93358942+jennalei02@users.noreply.github.com>
2023-10-22 11:58:23 -0700

    Update auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,11 +1,14 @@
+// get token to authenticate user when creating account or logging in
+
 import axios from 'axios';
 
 const API_KEY = 'AIzaSyAUuZUgvlmMYBOl_Kg43Eb2sHnEJp4_us4'
 
 async function authenticate(mode, email, password) {
+    
     const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
   
-    const response = await axios.post(url, {
+    const response = await axios.post( url, {
       email: email,
       password: password,
       returnSecureToken: true,
@@ -16,10 +19,10 @@ async function authenticate(mode, email, password) {
     return response.data;
   }
   
-  export function createUser(email, password) {
+  export function createUser( email, password ) {
     return authenticate('signUp', email, password);
   }
   
-  export function login(email, password) {
-    return authenticate('signInWithPassword', email, password);
+  export function login( email, password ) {
+    return authenticate( 'signInWithPassword', email, password );
   }
